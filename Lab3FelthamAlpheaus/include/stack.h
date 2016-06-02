17b9936760f9e331cc748e90cafe0ec5a73fd304
Emraldis <alfie.feltham@gmail.com>
2016-06-02 12:57:02 -0700

    forgot to make previous and top pointers pointers

--- Lab3FelthamAlpheaus/include/stack.h
+++ Lab3FelthamAlpheaus/include/stack.h
@@ -3,8 +3,8 @@
 
 typedef struct Stackinfo{
 	char * value;
-	struct Stackinfo previous;
-	struct Stackinfo top;
+	struct Stackinfo * previous;
+	struct Stackinfo * top;
 }Stack;
 
 Stack * createStack(char * value);
