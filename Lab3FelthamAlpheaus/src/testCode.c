9b0d22907fa673258c97a8ccc301caa474e78671
Emraldis <alfie.feltham@gmail.com>
2016-06-02 13:48:44 -0700

    Updated readme, and removed getchar(); from the testcode, as it was no longer necessary

--- Lab3FelthamAlpheaus/src/testCode.c
+++ Lab3FelthamAlpheaus/src/testCode.c
@@ -19,43 +19,29 @@ int main(void){
 	}
 	
 	printf("\nThe value of the element on top of the stack is: %s",getTopValue(stack));
-	getchar();
 	printf("\nPrinting the stack");
-	getchar();
 	printStack(stack);
 	printf("\nRemoving the top element of the stack, and printing again");
-	getchar();	
 	stack = removeFromTop(stack);
 	printStack(stack);
 	printf("\nThe value of the element on top of the stack is: %s",getTopValue(stack));
-	getchar();
 	printf("\nDeleting the stack, and then printing again");
-	getchar();	
 	stack = deleteStack(stack);
 	printStack(stack);
 	printf("\nThe value of the element on top of the stack is: %s",getTopValue(stack));
-	getchar();
 	printf("\nAttempting to delete an empty stack");
-	getchar();
 	stack = deleteStack(stack);
 	printf("\nAttempting to remove the top element of an empty stack");
-	getchar();
 	stack = removeFromTop(stack);
 	printf("\nAttempting to add an element to an empty stack");
-	getchar();	
 	stack = addToStack("1",stack);	
 	printf("\nThe value of the element on top of the stack is: %s",getTopValue(stack));
-	getchar();
 	printf("\nPrinting the stack");
-	getchar();	
 	printStack(stack);
 	printf("\nAdding an element with an empty value");
-	getchar();
 	stack = addToStack(NULL,stack);
 	printf("\nThe value of the element on top of the stack is: %s",getTopValue(stack));
-	getchar();
-	printf("\nPrinting the stack");
-	getchar();	
+	printf("\nPrinting the stack");	
 	printStack(stack);
 	printf("\n");
 	
