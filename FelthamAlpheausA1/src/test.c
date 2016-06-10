2fca469c8836012e69f6daf8b58f8bfd8942aa20
Emraldis <alfie.feltham@gmail.com>
2016-06-10 13:30:33 -0700

    quick function tests

--- FelthamAlpheausA1/src/test.c
+++ FelthamAlpheausA1/src/test.c
@@ -17,20 +17,24 @@ int main(void){
 	dataPtr = NULL;
 	
 	printf("\nTesting List ADT:");
-	getchar();
+	//getchar();
 	printf("\nCreating List");
 	for(i=1;i<101;i++){
 		sprintf(string,"%d",i);
 		data = create(string);
 		list = addToFront(data,list,sizeof(data));
 	}
-	getchar();
+	//getchar();
 	printf("\nPrinting List:");
 	currentPos = list;
 	while(currentPos != NULL){
 		printData(currentPos->structPtr);
 		currentPos = currentPos->next;
 	}
+	printf("\nFirst value is:");
+	printData(getValueFront(list));
+	printf("\nLast value is:");
+	printData(getValueBack(list));
 	
 	return(0);
 }
