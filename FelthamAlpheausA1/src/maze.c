eea123b62e2d0b63d9b389461e4d87fb4d8449e8
Emraldis <alfie.feltham@gmail.com>
2016-06-10 15:16:59 -0700

    added more maze stuff

--- FelthamAlpheausA1/src/maze.c
+++ FelthamAlpheausA1/src/maze.c
@@ -9,20 +9,36 @@
 int main(void){
 	FILE * mazeData;
 	char * string;
+	char * * maze;
+	char * mazeLayer;
+	int sizeX = 0;
+	int sizeY = 0;
 	int i = 0;
+	int j = 0;
+	int startX;
+	int startY;
 	
 	mazeData = fopen("maze","r");
 	string = malloc(sizeof(char) * 128);
+	mazeLayer = malloc(sizeof(char));
+	maze = malloc(sizeof(char *));
 	
 	printf("\nopening maze file");
-	getchar();
 	if(mazeData != NULL){
-		while ((fgets(string,128,mazeData) != NULL) && (i<100)){
-			printf("%s",string);
+		while ((fgets(string,128,mazeData) != NULL) && (i<101)){
+			if(strlen(string) > sizeX){
+				sizeX = strlen(string);
+			}
+			realloc(mazeLayer,sizeof(char) * sizeX);
+			strcpy(mazeLayer,string);
+			sizeY++;
+			realloc(maze,sizeof(char) * sizeY);
+			strcpy(maze[i],mazeLayer);
+			printf("%s",maze[i]);
 			i++;
 		}
 	}else{
-		printf("ERROR opening file");
+		printf("\nERROR opening file");
 	}
 	printf("\nDone");
 	
