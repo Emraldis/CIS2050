8834e9d970b4ab2046731b5f60552edbbe9d141b
Emraldis <alfie.feltham@gmail.com>
2016-06-23 19:16:28 -0700

    finalizing

--- Lab6FelthamAlpheaus/src/generator.c
+++ Lab6FelthamAlpheaus/src/generator.c
@@ -12,10 +12,12 @@ int main(void){
 	
 	tempString = malloc(sizeof(char) * 256);
 	
+	outputFile = fopen("numList", "w");
+	
 	for(i=0;i<300;i++){
 		tempInt = (rand() % 3000000);
 		sprintf(tempString,"%d\n",tempInt);
+		fputs(tempString,outputFile);
 	}
-	
-	outputFile = fopen("numList", "w");
+	fclose(outputFile);
 }
