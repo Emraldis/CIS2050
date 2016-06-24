8f1dbb77283b5b11f1e885678aea84a4fd48d116
Emraldis <alfie.feltham@gmail.com>
2016-06-23 19:07:54 -0700

    update

---
+++ Lab6FelthamAlpheaus/src/generator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	FILE * outputFile;
+	srand(time_t);
+	char * tempString;
+	int i;
+	int tempInt;
+	
+	tempString = malloc(sizeof(char) * 256);
+	
+	for(i=0;i<300;i++){
+		tempInt = (rand() % 3000000);
+		sprintf(tempString,"%d\n",tempInt);
+	}
+	
+	outputFile = fopen("numList", "w");
+}
