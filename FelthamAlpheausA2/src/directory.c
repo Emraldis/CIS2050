8b5bbcbdf77a37253aeb8fe49263527b2f8036b7
Emraldis <alfie.feltham@gmail.com>
2016-07-10 00:44:46 -0700

    test

--- FelthamAlpheausA2/src/directory.c
+++ FelthamAlpheausA2/src/directory.c
@@ -63,31 +63,31 @@ double decode(char * phoneNum){
 				output = (output + 0);
 				break;
 			case '1':
-				output = (output + (1 * pwr(10,i)));
+				output = (output + (1 * pwr(10,(i-1))));
 				break;
 			case '2':
-				output = (output + (2 * pwr(10,i)));
+				output = (output + (2 * pwr(10,(i-1))));
 				break;
 			case '3':
-				output = (output + (3 * pwr(10,i)));
+				output = (output + (3 * pwr(10,(i-1))));
 				break;
 			case '4':
-				output = (output + (4 * pwr(10,i)));
+				output = (output + (4 * pwr(10,(i-1))));
 				break;
 			case '5':
-				output = (output + (5 * pwr(10,i)));
+				output = (output + (5 * pwr(10,(i-1))));
 				break;
 			case '6':
-				output = (output + (6 * pwr(10,i)));
+				output = (output + (6 * pwr(10,(i-1))));
 				break;
 			case '7':
-				output = (output + (7 * pwr(10,i)));
+				output = (output + (7 * pwr(10,(i-1))));
 				break;
 			case '8':
-				output = (output + (8 * pwr(10,i)));
+				output = (output + (8 * pwr(10,(i-1))));
 				break;
 			case '9':
-				output = (output + (9 * pwr(10,i)));
+				output = (output + (9 * pwr(10,(i-1))));
 				break;
 			default:
 			printf("\nERROR DECODING");
@@ -111,7 +111,7 @@ double pwr(int base, int exponent){
 	}else if(exponent == 1){
 		return(base);
 	}
-	for(i=0;i<(exponent - 2);i++){
+	for(i=0;i<(exponent - 1);i++){
 		output = (output * base);
 	}
 
