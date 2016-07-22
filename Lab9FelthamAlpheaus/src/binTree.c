997dfa75a2e6a162f8cee44235942f53724a089b
Emraldis <alfie.feltham@gmail.com>
2016-07-21 22:08:42 -0700

    quick test

--- Lab9FelthamAlpheaus/src/binTree.c
+++ Lab9FelthamAlpheaus/src/binTree.c
@@ -47,7 +47,7 @@ Branch * addBranch(Branch * root, Branch * toBeAdded,int (* compare) (void * dat
 }
 
 void traverseTree (Branch * root, void (* run) (void * data)){
-	
+	void (* usrFunc) (void * data) = &run;
 }
 
 void removeBranch (Branch * root, void * searchData, int (* compare) (void * data1, void * data2), void (* deleteData) (void const *)){
