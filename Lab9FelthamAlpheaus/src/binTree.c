9f5c796131d04b55ee4ec8f559eb322b8c5b50f1
Emraldis <alfie.feltham@gmail.com>
2016-07-22 19:25:11 -0700

    formatting test

--- Lab9FelthamAlpheaus/src/binTree.c
+++ Lab9FelthamAlpheaus/src/binTree.c
@@ -67,15 +67,20 @@ void traverseTree (Branch * root, void (* run) (void const *),int depth){
 	currentBranch = malloc(sizeof(Branch));
 	
 	currentBranch = root;
+	if(run != NULL){
+		for(i=0;i<depth;i++){
+			printf("\t");
+		}
+	}
 	if(currentBranch != NULL){
-		traverseTree(currentBranch->rightBranch,run,(depth + 1));
 		printf("/");
+		traverseTree(currentBranch->rightBranch,run,(depth + 1));
 		if(run != NULL){
-			if(run != NULL){
+			/*if(run != NULL){
 				for(i=0;i<depth;i++){
 					printf("\t");
 				}
-			}
+			}*/
 			printf("-");
 			run(currentBranch->data);
 			printf("\n");
@@ -84,12 +89,12 @@ void traverseTree (Branch * root, void (* run) (void const *),int depth){
 		traverseTree(currentBranch->leftBranch,run,(depth + 1));
 	}else{
 		if(run != NULL){
-			if(run != NULL){
+			/*if(run != NULL){
 				for(i=0;i<depth;i++){
 					printf("\t");
 				}
-			}
-			printf("EMPTY");
+			}*/
+			printf("-EMPTY");
 			printf("\n");
 		}
 	}
